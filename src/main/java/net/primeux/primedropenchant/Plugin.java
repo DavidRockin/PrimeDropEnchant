c0b1024f7ab3cb1b794f3f705ff12bf5397f323b
Dave <DavidRockin@users.noreply.github.com>
2019-06-30 22:55:32 -0400

    init plugin, vault

---
+++ src/main/java/net/primeux/primedropenchant/Plugin.java
@@ -0,0 +1,18 @@
+package net.primeux.primedropenchant;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin
+{
+
+	@Override
+	public void onEnable()
+	{
+	}
+
+	@Override
+	public void onDisable()
+	{
+	}
+
+}
